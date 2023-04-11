d47ec19ea525b021d2e17ea2a0c64e31447b8e5d
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-11 19:58:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import ranksums
 
 
 chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.09
-    stat, p = ks_2samp(x, y)
+    stat, p = ranksums(x, y)
     if p <= alpha:
         return True
     else:
