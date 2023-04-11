fe3048e8c3e74a6212bdaa391e52e3299595285e
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-11 18:48:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.09
+    t_stat, p_value = ttest_ind(sample1, sample2)
+    if p_value <= alpha:
+        return True
+    else:
+        return False
