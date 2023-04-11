13a02682b819df20bbcf197174af2949a568d2f0
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-11 20:32:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ranksums
+from scipy.stats import anderson_ksamp
 
 
 chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.09
-    stat, p = ranksums(x, y)
-    if p <= alpha:
+    stat, critical_values, significance_levels = anderson_ksamp([x, y])
+    p_value = significance_levels[np.where(critical_values >= stat)[0][0]]
+    if p_value <= alpha:
         return True
     else:
         return False
