10bb69dfa5f693380d28fa6163a113ae452f67ca
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-11 19:08:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.09
-    t_stat, p_value = ttest_ind(x, y)
-    if p_value <= alpha:
+    stat, p = mannwhitneyu(x, y, alternative='two-sided')
+    if p <= alpha:
         return True
     else:
         return False
